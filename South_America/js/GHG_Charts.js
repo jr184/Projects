2f36d12e75071e3630e6f9ad384c768d87c35530
jr184 <jr184@humboldt.edu>
2017-02-18 19:10:32 -0800

    Add files via upload

---
+++ South_America/js/GHG_Charts.js
@@ -0,0 +1,469 @@
+function GetChartArgentina()
+{
+	var ArgentinaData = new Array([2009, 389.4890836], [2010, 389.3786065], [2011, 394.6995492], [2012, 405.0347549]);
+	var myChart = new JSChart('graph1', 'line');
+	myChart.setDataArray(ArgentinaData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 389.49']);
+	myChart.setTooltip([2010, 'GHG 389.38']);
+	myChart.setTooltip([2011, 'GHG 394.70']);
+	myChart.setTooltip([2012, 'GHG 405.03']);
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	//myChart.setSize(616, 321);
+	//myChart.setBackgroundImage('chart_bg.jpg');
+	myChart.draw();
+}
+
+function GetChartBrazil()
+{
+	var BrazilData = new Array([2009, 128.7750143], [2010, 154.8518126], [2011, 140.2844785], [2012, 136.4737383]);
+	var myChart = new JSChart('graph2', 'line');
+	myChart.setDataArray(BrazilData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 128.78']);
+	myChart.setTooltip([2010, 'GHG 154.85']);
+	myChart.setTooltip([2011, 'GHG 140.28']);
+	myChart.setTooltip([2012, 'GHG 136.47']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}	
+
+function GetChartBolivia()
+{
+	var BoliviaData = new Array([2009, 1661.520019], [2010, 1773.77442], [2011, 1768.753706], [2012, 1823.148349]);
+	var myChart = new JSChart('graph3', 'line');
+	myChart.setDataArray(BoliviaData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 1661.52']);
+	myChart.setTooltip([2010, 'GHG 1773.77']);
+	myChart.setTooltip([2011, 'GHG 1768.75']);
+	myChart.setTooltip([2012, 'GHG 1823.15']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();		
+}
+
+function GetChartChile()
+{
+	var ChileData = new Array([2009, 80.42961906], [2010, 84.90961679], [2011, 91.70165317], [2012, 93.73860728]);
+	var myChart = new JSChart('graph4', 'line');
+	myChart.setDataArray(ChileData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 80.43']);
+	myChart.setTooltip([2010, 'GHG 84.91']);
+	myChart.setTooltip([2011, 'GHG 91.70']);
+	myChart.setTooltip([2012, 'GHG 93.74']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+
+function GetChartColumbia()
+{
+	var ColumbiaData = new Array([2009, 196.1294622], [2010, 199.0214231], [2011, 202.1179626], [2012, 199.6809447]);
+	var myChart = new JSChart('graph5', 'line');
+	myChart.setDataArray(ColumbiaData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 196.13']);
+	myChart.setTooltip([2010, 'GHG 199.02']);
+	myChart.setTooltip([2011, 'GHG 202.12']);
+	myChart.setTooltip([2012, 'GHG 199.68']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+
+function GetChartEcuador()
+{
+	var EcuadorData = new Array([2009, 133.917459], [2010, 136.8023866], [2011, 137.0468221], [2012, 138.1583334]);
+	var myChart = new JSChart('graph6', 'line');
+	myChart.setDataArray(EcuadorData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 133.92']);
+	myChart.setTooltip([2010, 'GHG 136.80']);
+	myChart.setTooltip([2011, 'GHG 137.05']);
+	myChart.setTooltip([2012, 'GHG 138.16']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+
+function GetChartGuyana()
+{
+	var GuyanaData = new Array([2009, 7.49714964], [2010, 7.081320668], [2011, 7.191482864], [2012, 7.63708306]);
+	var myChart = new JSChart('graph7', 'line');
+	myChart.setDataArray(GuyanaData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 7.50']);
+	myChart.setTooltip([2010, 'GHG 7.08']);
+	myChart.setTooltip([2011, 'GHG 7.19']);
+	myChart.setTooltip([2012, 'GHG 7.64']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+
+function GetChartGuyane()
+{
+	var GuyaneData = new Array([2009, 1661.520019], [2010, 1773.77442], [2011, 1768.753706], [2012, 1823.148349]);
+	var myChart = new JSChart('graph8', 'line');
+	myChart.setDataArray(GuyaneData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 1661.52']);
+	myChart.setTooltip([2010, 'GHG 1773.77']);
+	myChart.setTooltip([2011, 'GHG 1768.75']);
+	myChart.setTooltip([2012, 'GHG 1823.15']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+	
+function GetChartParaguay()
+{
+	var ParaguayData = new Array([2009, 105.332524], [2010, 107.9307942], [2011, 109.4061477], [2012, 110.9796333]);
+	var myChart = new JSChart('graph9', 'line');
+	myChart.setDataArray(ParaguayData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 105.33']);
+	myChart.setTooltip([2010, 'GHG 107.93']);
+	myChart.setTooltip([2011, 'GHG 109.41']);
+	myChart.setTooltip([2012, 'GHG 110.98']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+
+function GetChartPeru()
+{
+	var PeruData = new Array([2009, 151.2016936], [2010, 155.766933], [2011, 158.5160777], [2012, 159.4984521]);
+	var myChart = new JSChart('graph10', 'line');
+	myChart.setDataArray(PeruData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 151.20']);
+	myChart.setTooltip([2010, 'GHG 155.77']);
+	myChart.setTooltip([2011, 'GHG 158.57']);
+	myChart.setTooltip([2012, 'GHG 159.50']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+
+function GetChartSuriname()
+{
+	var SurinameData = new Array([2009, 8.887038517], [2010, 8.59503349], [2011, 8.119955327], [2012, 8.630397164]);
+	var myChart = new JSChart('graph11', 'line');
+	myChart.setDataArray(SurinameData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 8.89']);
+	myChart.setTooltip([2010, 'GHG 8.60']);
+	myChart.setTooltip([2011, 'GHG 8.12']);
+	myChart.setTooltip([2012, 'GHG 8.63']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+
+function GetChartUruguay()
+{
+	var UruguayData = new Array([2009, 15.84404429], [2010, 13.32425626], [2011, 14.81279526], [2012, 14.75941027]);
+	var myChart = new JSChart('graph12', 'line');
+	myChart.setDataArray(UruguayData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 15.84']);
+	myChart.setTooltip([2010, 'GHG 13.32']);
+	myChart.setTooltip([2011, 'GHG 14.81']);
+	myChart.setTooltip([2012, 'GHG 14.76']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();	
+}
+
+function GetChartVenezuela()
+{
+	var VenezuelaData = new Array([2009, 385.179108], [2010, 391.5631277], [2011, 372.2704837], [2012, 396.7376293]);
+	var myChart = new JSChart('graph13', 'line');
+	myChart.setDataArray(VenezuelaData);
+	myChart.setTitle('Total GHG Emissions');
+	myChart.setTitleColor('#FFFFFF');
+	myChart.setTitleFontSize(11);
+	myChart.setAxisNameX('Year');
+	myChart.setAxisNameY('');
+	myChart.setAxisColor('#00b33c');
+	myChart.setAxisValuesColor('#949494');
+	myChart.setAxisPaddingLeft(70);
+	myChart.setAxisPaddingRight(120);
+	myChart.setAxisPaddingTop(50);
+	myChart.setAxisPaddingBottom(40);
+	myChart.setAxisValuesDecimals(2); 
+	myChart.setAxisValuesNumberX(5);
+	myChart.setShowXValues(false);
+	myChart.setGridColor('#BBBBBB');
+	myChart.setLineColor('#BBBBBB');
+	myChart.setLineWidth(2);
+	myChart.setFlagColor('#00b33c');
+	myChart.setFlagRadius(4);
+	myChart.setTooltip([2009, 'GHG 385.18']);
+	myChart.setTooltip([2010, 'GHG 391.53']);
+	myChart.setTooltip([2011, 'GHG 372.27']);
+	myChart.setTooltip([2012, 'GHG 396.74']);		
+	myChart.setLabelX([2009, '2009']);
+	myChart.setLabelX([2010, '2010']);
+	myChart.setLabelX([2011, '2011']);
+	myChart.setLabelX([2012, '2012']);
+	myChart.setSize(411, 214);
+	myChart.draw();		
+}
