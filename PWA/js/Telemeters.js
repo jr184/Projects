508127f0691d108ed91a7414fdc0b10f4242ae11
jr184 <jr184@humboldt.edu>
2017-02-19 16:27:42 -0800

    Add files via upload

---
+++ PWA/js/Telemeters.js
@@ -0,0 +1,10 @@
+var Telemeters = {
+	"type": "FeatureCollection",																				
+	"features": [
+		{ "type": "Feature", "properties": { "name": "pwaDemoUnit5"}, "geometry": { "type": "Point", "coordinates": [ -124.03541, 40.92890] } },
+		{ "type": "Feature", "properties": { "name": "pwademoUnit6"}, "geometry": { "type": "Point", "coordinates": [ -123.94141, 40.94982] } },
+		{ "type": "Feature", "properties": { "name": "pwaMk1a"}, "geometry": { "type": "Point", "coordinates": [ -123.78110, 40.95262] } },
+		{ "type": "Feature", "properties": { "name": "pwaMk1B"}, "geometry": { "type": "Point", "coordinates": [ -123.82719, 40.86917] } },
+		{ "type": "Feature", "properties": { "name": "pwaMk1c"}, "geometry": { "type": "Point", "coordinates": [ -123.93205, 40.82887] } }
+	]
+};
